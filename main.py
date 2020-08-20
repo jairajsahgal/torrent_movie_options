b0d5bc401cf139efab3547a212cde616d6a19d62
Jairaj Sahgal <jairajiitaspirant@gmail.com>
2020-08-20 18:38:51 +0530

    Added mpv player

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import requests
 import subprocess
 import sys
 
-def command_caller(MagnetLink, download):
+def command_caller(MagnetLink, choice):
 	cmd=[]
 	cmd.append("webtorrent")
 	cmd.append(MagnetLink)
-	if not download:
+	if choice==1:
 		cmd.append("--vlc")
-	if sys.platform.startswith('linux'):
-		subprocess.call(cmd)
+	elif choice==2:
+		if sys.platform.startswith('linux'):
+			subprocess.call(cmd)
+	elif choice==3:
+		cmd.append("--mpv")
+	subprocess.call(cmd)
 def main():
 	name=input("Enter the movie name\t")
 	url=f"https://api.sumanjay.cf/torrent/?query={name}"
@@ -27,15 +31,17 @@ def main():
 
 	download=False
 
-	StreamChoice=int(input("1 for stream \n 2 for Download"))
+	StreamChoice=int(input("1 for stream on vlc \n 2 for Download \n 3 for stream on mpv"))
 	if StreamChoice==1:
 		download=False
 	elif StreamChoice==2:
 		download=True
+	elif StreamChoice==3:
+		download=False
 	else:
 		print("Wrong option choosen\nExiting.....")
 		exit(1)
-	command_caller(MagnetLink,download)
+	command_caller(MagnetLink,StreamChoice)
 
 
 
